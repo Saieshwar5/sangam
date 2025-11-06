3abae0badf4b84232921af144b12cf3c1f5b9971
sal_eshwar <saieshwargandrath5@gmail.com>
2025-11-06 11:40:59 +0530

    fixed mysql config

--- server/src/config/mySqlConfig.js
+++ server/src/config/mySqlConfig.js
@@ -1,7 +1,9 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const sequelize = new Sequelize('sangam', 'root', 'mysecretpassword', {
-  host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOST,
   dialect: 'mysql',
   // Control naming conventions
   define: {
